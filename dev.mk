e803891960f50800495441b06ae9a6a527453c7d
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-10 20:29:22 -0600

    create output file

--- dev.mk
+++ dev.mk
@@ -1,16 +1,22 @@
 # Development makefile
+SRC=main tands
+SAMPLE_INPUT=inputexample
+OUTPUT=*.log
+APP=prodcon
+ZIP_NAME=Assignment2.zip
 
 CFLAGS=-Wall -Wextra -Wpedantic
-
-SRC=main tands
 OBJ=$(SRC:%=build/%.o)
-
 BUILD=build
 
-all: prodcon
+all: $(APP) $(SAMPLE_INPUT)
 
-run: prodcon
-	./prodcon
+# example input given in the assignment specifications
+$(SAMPLE_INPUT):
+	echo "T4\nT2\nT1\nT1\nS9\nT5\nS1\nT1\nT1\nT1" > $@
+
+run: $(APP)
+	./$<
 
 prodcon: $(OBJ)
 	gcc $(CFLAGS) -o $@ $^
@@ -23,8 +29,8 @@ $(BUILD):
 
 zip: clean
 	$(MAKE) clean
-	zip -r ../Assignment2.zip .
+	zip -r ../$(ZIP_NAME) .
 
 clean:
-	rm -f prodcon
+	rm -f $(APP) $(SAMPLE_INPUT) $(OUTPUT)
 	rm -rf $(BUILD)
--- src/main.c
+++ src/main.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void parse_args(int* n_threads, char* filename, int argc, char const* argv[]) {
+// accept one or two command line arguments
+// prodcon nthreads <id>
+FILE* parse_args(int* nthreads, int argc, char const* argv[]) {
   if (argc < 2) {
-    // accept one or two command line arguments
-    // prodcon nthreads <id>
     fprintf(stderr, "Usage: prodcon N_THREADS [LOG_ID]\n");
     exit(2);
   }
-  *n_threads = atoi(argv[1]);
+  *nthreads = atoi(argv[1]);
+  if (*nthreads <= 0) {
+    *nthreads = 1;  // make sure that nthreads is a positive number
+  }
+
   int log_id = 0;
   if (argc >= 3) {
     log_id = atoi(argv[2]);
   }
+
+  char filename[64];
   if (log_id) {
     sprintf(filename, "prodcon.%d.log", log_id);
   } else {
+    // use default filename if input is missing/invalid
     sprintf(filename, "prodcon.log");
   }
 
-  printf("number of threads: %d, file name: %s\n", *n_threads, filename);
+  printf("number of threads: %d, file name: %s\n", *nthreads, filename);
+
+  FILE* fp = fopen(filename, "w");
+  if (fp == NULL) {
+    fprintf(stderr, "Error opening file");
+    exit(1);
+  }
+  return fp;
 }
 
 int main(int argc, char const* argv[]) {
-  int n_threads;
-  char filename[64];
-  parse_args(&n_threads, filename, argc, argv);
+  int nthreads;
+  FILE* fp = parse_args(&nthreads, argc, argv);
+  fclose(fp);
   return 0;
 }

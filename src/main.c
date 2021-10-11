2909d849119779dff7cba07f8236da1842449f2f
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-10 19:26:43 -0600

    parse command line args + format given code

--- src/main.c
+++ src/main.c
@@ -1,1 +1,30 @@
-int main() {}
+#include <stdio.h>
+#include <stdlib.h>
+
+void parse_args(int* n_threads, char* filename, int argc, char const* argv[]) {
+  if (argc < 2) {
+    // accept one or two command line arguments
+    // prodcon nthreads <id>
+    fprintf(stderr, "Usage: prodcon N_THREADS [LOG_ID]\n");
+    exit(2);
+  }
+  *n_threads = atoi(argv[1]);
+  int log_id = 0;
+  if (argc >= 3) {
+    log_id = atoi(argv[2]);
+  }
+  if (log_id) {
+    sprintf(filename, "prodcon.%d.log", log_id);
+  } else {
+    sprintf(filename, "prodcon.log");
+  }
+
+  printf("number of threads: %d, file name: %s\n", *n_threads, filename);
+}
+
+int main(int argc, char const* argv[]) {
+  int n_threads;
+  char filename[64];
+  parse_args(&n_threads, filename, argc, argv);
+  return 0;
+}
--- src/tands.c
+++ src/tands.c
@@ -2,8 +2,8 @@
 
 // Trans simulates processing a transaction
 // It does not simulate this by using sleep -- that would free up the CPU.
-// Thus, Trans does a silly computation to use up CPU cycles. Note that the 
-// computation has to produce some result. If it doesn't, then a smart 
+// Thus, Trans does a silly computation to use up CPU cycles. Note that the
+// computation has to produce some result. If it doesn't, then a smart
 // compiler will notice this and delete (optimize) the code!
 
 // The loop's computation (to waste time) is used to modify TransSave.
@@ -13,18 +13,17 @@
 
 int TransSave = 0;
 
-void Trans( int n ) {
-	long i, j;
+void Trans(int n) {
+  long i, j;
 
-	// Use CPU cycles 
-	j = 0;
-	for( i = 0; i < n * 100000; i++ ) {
-		j += i ^ (i+1) % (i+n);
-	}
-	TransSave += j;
-	TransSave &= 0xff;
+  // Use CPU cycles
+  j = 0;
+  for (i = 0; i < n * 100000; i++) {
+    j += i ^ (i + 1) % (i + n);
+  }
+  TransSave += j;
+  TransSave &= 0xff;
 }
-
 
 // Sleep simulates pauses between transactions
 // Parameter given expresses wait time in hundreds of a nanosecond.
@@ -32,18 +31,18 @@ void Trans( int n ) {
 #include <stdio.h>
 #include <time.h>
 
-void Sleep( int n ) {
-	struct timespec sleep;
+void Sleep(int n) {
+  struct timespec sleep;
 
-	// Make sure pass a valid nanosecond time to nanosleep
-	if( n <= 0 || n >= 100 ) {
-		n = 1;
-	}
+  // Make sure pass a valid nanosecond time to nanosleep
+  if (n <= 0 || n >= 100) {
+    n = 1;
+  }
 
-	// Sleep for less than one second
-	sleep.tv_sec  = 0;
-        sleep.tv_nsec = n * 10000000 + TransSave;
-	if( nanosleep( &sleep, NULL ) < 0 ) {
-		perror ("NanoSleep" );
-	}
+  // Sleep for less than one second
+  sleep.tv_sec = 0;
+  sleep.tv_nsec = n * 10000000 + TransSave;
+  if (nanosleep(&sleep, NULL) < 0) {
+    perror("NanoSleep");
+  }
 }
